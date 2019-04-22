df177ffc5f7c016497e673653b5b88fa5bd73ed8
tomiot1097 <tomiot1097@gmail.com>
2019-04-22 22:51:20 +0700

    dung co chay branch nay

--- resources/views/user/libraries/jsCode.blade.php
+++ resources/views/user/libraries/jsCode.blade.php
@@ -13,29 +13,14 @@
 
 <!-- FB -->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+{{-- <script>(function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
 	js = d.createElement(s); js.id = id;
 	js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.1';
 	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));</script> --}}
 
-<!-- Sản phẩm xem gần đây -->
-<!-- kiểm tra có local storage hay k -->
-<script>
-	// Check browser support
-	if (typeof(Storage) !== "undefined") {
-	  // Store
-	  localStorage.setItem("lastname", "Smith");
-	  // Retrieve
-	  document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-	} else {
-	  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-	}
-</script>
-
-<!-- thêm sản phảm vào mảng -->
 <script>
 	
 </script>
--- routes/botman.php
+++ routes/botman.php
@@ -6,4 +6,27 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
+
+
+$botman->hears('i want', function ($bot) {
+    $bot->reply('Give me more information!');
+});
+
+$botman->hears('i am ([0-9]+) years old', function ($bot,$age) {
+    $bot->reply('Got that, you are '.$age);
+});
+
+$botman->hears('.*Bonjour.*', function ($bot) {
+    $bot->reply('Nice to meet you!');
+});
+
+$botman->hears('Hey ([a-z]+)', function ($bot,$alpha) {
+    $bot->reply('You typed: '.$alpha);
+});
+
+$botman->fallback(function($bot){
+    $bot->reply("Please be nice to me!");
+});
+
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
