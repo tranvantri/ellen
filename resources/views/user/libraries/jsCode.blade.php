b5d72b6f81bf953d9b3e23e71415247cc84d4424
tomiot1097 <tomiot1097@gmail.com>
2019-04-23 09:56:40 +0700

    hi first commit

--- resources/views/user/libraries/jsCode.blade.php
+++ resources/views/user/libraries/jsCode.blade.php
@@ -11,31 +11,16 @@
 <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
 <script type="text/javascript" src="asset/js/myScript.js"></script>
 
-<!-- FB -->
-<div id="fb-root"></div>
+{{--  <div id="fb-root"></div>
 <script>(function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
 	js = d.createElement(s); js.id = id;
 	js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.1';
 	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));</script>  --}}
 
-<!-- Sản phẩm xem gần đây -->
-<!-- kiểm tra có local storage hay k -->
-<script>
-	// Check browser support
-	if (typeof(Storage) !== "undefined") {
-	  // Store
-	  localStorage.setItem("lastname", "Smith");
-	  // Retrieve
-	  document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-	} else {
-	  document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-	}
-</script>
 
-<!-- thêm sản phảm vào mảng -->
 <script>
 	
 </script>
