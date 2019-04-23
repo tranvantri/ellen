2936cc0e93a3b8c915707fc3c9f938c4122c15b8
tomiot1097 <tomiot1097@gmail.com>
2019-04-23 15:30:49 +0700

    commit something

--- routes/botman.php
+++ routes/botman.php
@@ -1,10 +1,9 @@
 <?php
 use App\Http\Controllers\BotManController;
 
-
-
 $botman = resolve('botman');
 
+/* Nhóm kịch bản đơn giản */
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello there!');
 });
@@ -19,7 +18,6 @@ $botman->hears('i am ([0-9]+)', function ($bot,$age) {
         'age'=>$age
     ]);
 });
-
 $botman->hears('how old am i', function ($bot) {
     $age = $bot->userStorage()->get('age');
     $bot->reply('You are '.$age.' years old.');
@@ -31,7 +29,6 @@ $botman->hears('my name is ([a-z]+)', function ($bot,$name) {
         'name'=>$name
     ]);
 });
-
 $botman->hears('what is my name', function ($bot) {
     $name = $bot->userStorage()->get('name');
     $bot->reply('Bạn là '.$name.' nè.');
@@ -46,8 +43,16 @@ $botman->hears('I want ([0-9]+) portions of (Cheese|Cake)', function ($bot, $amo
 
 });
 
+$botman->hears('my email is ([a-z0-9]+)', function ($bot,$email) {
+    $bot->reply('Oke. Your email is: '.$email);
+    $bot->userStorage()->save([
+        'email'=>$email
+    ]);
+});
 
-// send Messages to Controller
+/** -     -------     ------- Kết thúc nhóm Kịch bản đơn giản */
+
+/**  *-----------Nhóm kịch bản lấy dữ liệu từ DB ***************** */
 // xem danh sách nhóm sản phẩm
 $botman->hears('show cate group', 'App\Http\Controllers\UserController\ChatBoxController@handleGetTitles');
 
@@ -55,7 +60,9 @@ $botman->hears('show cate group', 'App\Http\Controllers\UserController\ChatBoxCo
 $botman->hears('show me {nameProduct}', 'App\Http\Controllers\UserController\ChatBoxController@handleGetCateProductID');
 
 
-//-------------------------------------------------------------------------------
+/***----------- Kết thúc Nhóm kịch bản lấy dữ liệu từ DB ***************** */
+
+//------------ Bộ câu hỏi ngoài xử lý   ------------------------------------
 $botman->fallback(function($bot){
     $bot->reply("Sorry i don't know what are you talking about!");
 });
